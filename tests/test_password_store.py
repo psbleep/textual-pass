4b738bcbc32ef4986fdfe371ff8c9a034e4a80d2
Patrick Schneeweis <psbleep@protonmail.com>
2022-02-06 12:00:38 -0700

    add password store tests

--- tests/test_password_store.py
+++ tests/test_password_store.py
@@ -1,9 +1,10 @@
 import os
 import shutil
+from unittest.mock import Mock
 
 import pytest
 
-from textual_pass.password_store import PasswordStore
+from textual_pass.password_store import SUBPROCESS_RUN_OPTIONS, PasswordStore
 
 from . import TEST_STORE_DIR
 
@@ -47,3 +48,55 @@ def test_search_returns_only_passwords_with_search_term_when_provided(store):
 
 def test_search_returns_empty_list_if_no_passwords_match_search_term(store):
     assert store.search("foobar") == []
+
+
+def test_run_shell_command_runs_command_and_returns_output(mocker, store):
+    mocked_subprocess_run = mocker.patch(
+        "textual_pass.password_store.subprocess.run",
+        return_value=Mock(stdout="hello", stderr=""),
+    )
+    assert store.run_shell_command("greet me") == "hello\n"
+    mocked_subprocess_run.assert_called_with("greet me", **SUBPROCESS_RUN_OPTIONS)
+
+
+def test_run_shell_command_returns_clean_output_when_stdout_and_stderr_both_present(
+    mocker, store
+):
+    mocker.patch(
+        "textual_pass.password_store.subprocess.run",
+        return_value=Mock(stdout="hello", stderr="error: do not know your name"),
+    )
+    assert store.run_shell_command("greet me") == "hello\nerror: do not know your name"
+
+
+def test_run_shell_command_returns_clean_output_if_no_stdout_only_stderr(mocker, store):
+    mocker.patch(
+        "textual_pass.password_store.subprocess.run",
+        return_value=Mock(stdout="", stderr="permission denied"),
+    )
+    # Checking that the output is not "\npermission denied" because of blank stdout
+    assert store.run_shell_command("hack fbi") == "permission denied"
+
+
+def test_show_password_opens_password_in_pass_and_returns_output(store):
+    store.run_shell_command = Mock(return_value="31337\n")
+    assert store.show_password("hello") == "31337\n"
+    store.run_shell_command.assert_called_with("pass hello")
+
+
+def test_show_otp_opens_otp_in_pass_and_returns_output(store):
+    store.run_shell_command = Mock(return_value="90827\n")
+    assert store.show_otp("hello") == "90827\n"
+    store.run_shell_command.assert_called_with("pass otp hello")
+
+
+def test_git_pull_runs_git_command_in_pass(store):
+    store.run_shell_command = Mock(return_value="Everything up-to-date.\n")
+    assert store.git_pull() == "Everything up-to-date.\n"
+    store.run_shell_command.assert_called_with("pass git pull")
+
+
+def test_git_push_runs_git_command_in_pass(store):
+    store.run_shell_command = Mock(return_value="Everything up-to-date.\n")
+    assert store.git_push() == "Everything up-to-date.\n"
+    store.run_shell_command.assert_called_with("pass git push")
--- textual_pass/password_store.py
+++ textual_pass/password_store.py
@@ -2,6 +2,8 @@ import os
 import subprocess
 from typing import List
 
+SUBPROCESS_RUN_OPTIONS = {"capture_output": True, "shell": True, "text": True}
+
 
 class PasswordStore:
     def __init__(self, password_store_dir: str) -> None:
@@ -20,24 +22,24 @@ class PasswordStore:
                         listing.append(listing_name)
         return sorted(listing)
 
-    def show_password(self, password: str) -> str:
-        cmd = f"pass {password}"
-        return self._run_shell_command(cmd)
-
-    def show_otp(self, password: str) -> str:
-        cmd = f"pass otp {password}"
-        return self._run_shell_command(cmd)
-
-    def git_pull(self) -> str:
-        return self._run_shell_command("pass git pull")
-
-    def git_push(self) -> str:
-        return self._run_shell_command("pass git push")
-
-    def _run_shell_command(self, cmd: str) -> str:
-        result = subprocess.run(cmd, capture_output=True, shell=True, text=True)
+    def run_shell_command(self, cmd: str) -> str:
+        result = subprocess.run(cmd, **SUBPROCESS_RUN_OPTIONS)
         if result.stdout:
             stdout = f"{result.stdout}\n"
         else:
             stdout = ""
         return f"{stdout}{result.stderr}"
+
+    def show_password(self, password: str) -> str:
+        cmd = f"pass {password}"
+        return self.run_shell_command(cmd)
+
+    def show_otp(self, password: str) -> str:
+        cmd = f"pass otp {password}"
+        return self.run_shell_command(cmd)
+
+    def git_pull(self) -> str:
+        return self.run_shell_command("pass git pull")
+
+    def git_push(self) -> str:
+        return self.run_shell_command("pass git push")

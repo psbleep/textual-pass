2010b48170645af0355dd894f59b4ed3494d8bb3
Patrick Schneeweis <psbleep@protonmail.com>
2022-02-07 18:58:09 -0700

    tests for app

---
+++ tests/test_app.py
@@ -0,0 +1,20 @@
+from unittest.mock import Mock
+
+import pytest
+import pytest_asyncio
+
+from textual_pass.app import App
+
+
+@pytest_asyncio.fixture
+async def app():
+    obj = App()
+    obj.store = Mock()
+    obj.passwords = Mock()
+    return obj
+
+
+@pytest.mark.asyncio
+async def test_handle_search_on_change(app):
+    message = Mock(sender=Mock(value=Mock(return_value="hello")))
+    await app.handle_search_on_change(message)

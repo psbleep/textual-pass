aa469c88601f6e33927673611420a196070b2f7f
Patrick Schneeweis <psbleep@protonmail.com>
2022-02-08 09:43:52 -0700

    verify password store dir used in tests

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,4 +1,13 @@
 import os
 
+from textual_pass import PASSWORD_STORE_DIR
+
+
 TESTS_DIR = os.path.dirname(__file__)
 TEST_STORE_DIR = os.path.join(TESTS_DIR, ".test-password-store")
+
+if PASSWORD_STORE_DIR != TEST_STORE_DIR:
+    raise OSError(
+        "PASSWORD_STORE_DIR env variable not set to expected value for tests!\n"
+        f"Expected path: {TEST_STORE_DIR}\nActual path: {PASSWORD_STORE_DIR}"
+    )
--- textual_pass/__init__.py
+++ textual_pass/__init__.py
@@ -6,4 +6,6 @@ def get_default_password_store_dir():
     return os.path.join(home_dir, ".password-store")
 
 
-PASSWORD_STORE_DIR = os.getenv("PASSWORD_STORE_DIR") or get_default_password_store_dir()
+PASSWORD_STORE_DIR = os.path.abspath(
+    os.getenv("PASSWORD_STORE_DIR") or get_default_password_store_dir()
+)
